e759a97bbf29362390236ae9667d1bea3b1f8530
Michael Stein <info@michaelstein-itb.de>
2023-08-19 07:51:48 +0200

    add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,13 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'mst yaml to TCA loader',
+    'description' => 'Load contentelements from yaml in register in new element wizard',
+    'state' => 'stable',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99'
+        ],
+    ],
+];
