f0bc3dde675719f4b23963eecf124b83c07e1b1d
Michael Stein <info@michaelstein-itb.de>
2024-11-28 18:23:10 +0100

    [RELEASE] extensions/mst_yaml2tca 1.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,10 +4,5 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'YAML to TCA loader',
     'description' => 'Load contentelements from yaml in register in new element wizard',
     'state' => 'alpha',
-    'version' => '1.0.3',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '11.5.0-13.4.1'
-        ],
-    ],
+    'version' => '1.0.4'
 ];
