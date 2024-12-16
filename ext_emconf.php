56bfd7ccdb463448a20c4df41a22218647dc248c
Michael Stein <info@michaelstein-itb.de>
2024-12-16 15:09:14 +0100

    update extension configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,10 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'YAML to TCA loader',
     'description' => 'Load contentelements from yaml in register in new element wizard',
-    'state' => 'alpha',
+    'state' => 'stable',
+    'author' => 'Michael Stein (mxsteini)',
+    'author_email' => 'info@michaelstein-itb.de',
+    'author_company' => 'Michael Stein IT-Beratung',
     'version' => '1.0.8',
     'constraints' => [
         'depends' => [
